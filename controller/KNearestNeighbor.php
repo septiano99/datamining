9e48765f704284a13d4cdcd2b22dcbc4cc5dd2a5
dicky.akbar@dwidasa.com <akbar128256512>
2020-05-14 21:42:41 +0700

    commit

--- controller/KNearestNeighbor.php
+++ controller/KNearestNeighbor.php
@@ -156,8 +156,8 @@
 //                    $test[6], $test[7], $test[8], $test[9], $test[10], $test[11], $test[12], '$test[13]', '$test[14]',
 //                    $test[15], $test[16], $test[17], $test[18], $test[19], '$test[20]', $test[21])";
 
-            $sql = "INSERT INTO t_result (kolek, no_cif, tgl_lahir, jenis_kelamin) VALUES
-                    ($resultData, '$test[0]', '$test[1]', $test[2])";
+            $sql = "INSERT INTO t_result (kolek, no_cif, tgl_lahir, jenis_kelamin, tgl_buka, tgl_jt) VALUES
+                    ($resultData, '$test[0]', '$test[1]', $test[2], '$test[3]', '$test[4]')";
 
             $result = pg_query($sql);
         }

ed0f6050fddffa90ade068282c06198ab33e94a3
dicky.akbar@dwidasa.com <akbar128256512>
2020-05-17 16:29:26 +0700

    commit

--- controller/NaiveBayes.php
+++ controller/NaiveBayes.php
@@ -160,25 +160,13 @@
             $resultData = 1;
             foreach ($resultProdCi as $key => $value) {
                 $finalCount = ($resultMultiplyProbXCi[$key][0] * $value[0]) + 2 / 1;
-//                $finalCount = 100 - $finalCount;
+                $finalCount = 100 - $finalCount;
 
                 if ($compare < $finalCount) {
                     $compare = $finalCount;
                     $resultData = $key;
                 }
-
-//                $finalProb[$key] = array();
-//                array_push($finalProb, $finalCount);
             }
-
-            // Mencari probabilitas tertinggi
-
-//            foreach ($finalProb as $key => $value) {
-//                if ($compare < $value[0]) {
-//                    $compare = $value[0];
-//                    $resultData = $key;
-//                }
-//            }
 
             $test = $testingOriginal[$index];
             $sql = "INSERT INTO t_result (kolek, no_cif, tgl_lahir, jenis_kelamin, tgl_buka, tgl_jt, jwaktu_bln,

6528d5256a4e16d1694b9e5cba5bcbcc1005cb5c
dicky.akbar@dwidasa.com <akbar128256512>
2020-05-20 13:34:13 +0700

    commit

--- controller/Connection.php
+++ controller/Connection.php
@@ -1,8 +1,8 @@
 <?php
-    $host = "ec2-18-232-143-90.compute-1.amazonaws.com";
-    $dbName = "dbvnr5qt9dco7q";
-    $user = "mhwokbyoflkkvc";
-    $password = "1749b4963d9b4bd285e939bd82d382c95ae7b9ca5ecbfc4b570eba0fa89ec7e1";
+    $host = "ec2-54-161-208-31.compute-1.amazonaws.com";
+    $dbName = "dl7s8o74qnp8k";
+    $user = "tqlccckautcmwv";
+    $password = "252c19eee6936f6e91959793ed87619f9071092272e34c42ecd20f8d9aba59ef";
     $db = pg_connect("host=$host port=5432 dbname=$dbName user=$user password=$password");
     echo "<script>console.log('Connection DB " . $dbName . " Success' );</script>";
 ?>
